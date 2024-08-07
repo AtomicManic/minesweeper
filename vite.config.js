5594f359d121af1f7323fa0355b8976ceb6c4f56
adva <adva.apelboim@gmail.com>
2024-08-08 01:28:59 +0300

    alter-final-files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
+  plugins: [react(), vercel()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
-  plugins: [vercel()],
+  server: {
+    port: process.env.PORT || 3000,
+  },
 });
